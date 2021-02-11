bf7048abb488a27ae7e8664d6dd2ba734a75ca55
Adamshw <adam.shwehne@gmail.com>
2021-02-11 10:25:42 +0100

    first

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CountButton from './CountButton'
+
+const App = () => {
+  
+  return (
+      <div>
+        <CountButton/>
+      </div>
+  )
+}
+
+//App code will be available for other files
+export default App
